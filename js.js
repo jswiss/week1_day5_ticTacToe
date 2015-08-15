6e8c4b4462dcb3ec994a4671bc9a9657a24c8819
Joshua Swiss <swissjoshua@gmail.com>
2015-08-15 13:23:25 +0100

    basic html/css almost done - responsive to window resize

---
+++ js.js
@@ -0,0 +1,10 @@
+//consider storing the contents of the board in an array. Can you represent the winning combinations in another array? You could then check if any of the winning combinations are on the board after every move.
+
+  //above makes sense x = 1, o = 2, []
+
+  //a cell should not be able to be played once it has been marked
+  //refresh button to reset once someone wins? or time limit -->
+
+  //keep a tally of who has won how many times
+
+  

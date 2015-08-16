910d50418b44544b8fde1da340c71921a5a6a924
Joshua Swiss <swissjoshua@gmail.com>
2015-08-16 21:51:14 +0100

    some changes to record clicks

--- js.js
+++ js.js
@@ -1,45 +1,52 @@
-var beginGame = function () {
+console.log('linked');
+
   var boxes = document.getElementsByClassName('boxes');
-  var playerTurn = 'x'
+  var box = document.getElementById('box7');
+  console.log(boxes);
+  var nextTurn = 'x'
+  var showMoves = document.getElementsByClassName('boxes')[i].innerHTML = nextTurn;
 
-  boxes.addEventListener('click', function () {
-    for (i = 0; i < boxes.length; i++) {
-      document.getElementsByClassName('row')[i].innerHTML = playerTurn;
-    }
-  });
-      
-
-  var boxesContent = [];
-  for (var i = 0; i < boxes.length; i++) {
-    boxesContent.push(boxes[i].innerHtml);
-  };
-
-    var winner = function() {
-      if ( (boxesContent[0] === boxesContent[1] && boxesContent[1] === boxesContent[2]) ||
-         (boxesContent[3] === boxesContent[4] && boxesContent[4] === boxesContent[5]) ||
-         (boxesContent[6] === boxesContent[7] && boxesContent[7] === boxesContent[8]) ||
-         (boxesContent[0] === boxesContent[3] && boxesContent[3] === boxesContent[6]) ||
-         (boxesContent[1] === boxesContent[4] && boxesContent[4] === boxesContent[7]) ||
-         (boxesContent[2] === boxesContent[5] && boxesContent[5] === boxesContent[8]) ||
-         (boxesContent[0] === boxesContent[4] && boxesContent[4] === boxesContent[8]) ||
-         (boxesContent[2] === boxesContent[4] && boxesContent[4] === boxesContent[6]) ) {
-
-
-  function playerTurn() {
-      if (nextTurn === 'x') {
-          nextTurn === 'o';
-      } else {
-          nextTurn = 'x';
-      };
-
-  };
+function beginGame() {
+  addEventListener();
 }
 
-beginGame;
+function addEventListener(){
+  for (var i = boxes.length - 1; i >= 0; i--) {
+    // console.log(boxes, [i]);
+    boxes[i].addEventListener("click", storeMoves);
+    boxes[i].addEventListener("click", showMoves);
+  }
+}
+
+function storeMoves() {
+  console.log(this);
+}
+
+beginGame();
+
+  // var boxesContent = [];
+  // for (var i = 0; i < boxes.length; i++) {
+  //   boxesContent.push(boxes[i].innerHtml);
+  // };
+
+//     var winner = function() {
+//       if ( (boxesContent[0] === boxesContent[1] && boxesContent[1] === boxesContent[2]) ||
+//          (boxesContent[3] === boxesContent[4] && boxesContent[4] === boxesContent[5]) ||
+//          (boxesContent[6] === boxesContent[7] && boxesContent[7] === boxesContent[8]) ||
+//          (boxesContent[0] === boxesContent[3] && boxesContent[3] === boxesContent[6]) ||
+//          (boxesContent[1] === boxesContent[4] && boxesContent[4] === boxesContent[7]) ||
+//          (boxesContent[2] === boxesContent[5] && boxesContent[5] === boxesContent[8]) ||
+//          (boxesContent[0] === boxesContent[4] && boxesContent[4] === boxesContent[8]) ||
+//          (boxesContent[2] === boxesContent[4] && boxesContent[4] === boxesContent[6]) ) {
 
 
+function playerTurn() {
+  if (nextTurn === 'x') {
+      nextTurn === 'o';
+  } else {
+      nextTurn = 'x';
+  } 
+};
 
-        
-  
-
+playerTurn();
 
